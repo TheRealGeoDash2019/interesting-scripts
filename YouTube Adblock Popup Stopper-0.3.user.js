f1620476a0152f0dd13b72f7f3adda741b15e1e7
Travis Silva <60702045+TheRealGeoDash2019@users.noreply.github.com>
2023-11-10 19:59:13 -0600

    Add files via upload

---
+++ YouTube Adblock Popup Stopper-0.3.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         YouTube Adblock Popup Stopper
+// @version      0.3
+// @description  Simple script that removes the popup that appears on YouTube about AdBlock
+// @author       TheRealGeoDash
+// @match        https://www.youtube.com/watch*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    (new MutationObserver(function(ms, os) {
+        for (const m of ms) {
+            if (m.type === "childList") {
+                if (!"parentElement" in m.target) return;
+                const aP = m.target?.parentElement?.querySelector("ytd-enforcement-message-view-model");
+                if (aP) {
+                    console.log("Purging AdBlock Popup");
+                    aP.remove();
+                    setTimeout(() => {
+                        console.log("Purging AdBlock Overlay");
+                        document.querySelector(`tp-yt-paper-dialog[prevent-autonav="true"]`).remove();
+                        os.disconnect();
+                        if (document.querySelector("video").paused) {
+                            console.log("Attempting to Resume Playback :)");
+                            document.querySelector("video").play();
+                        }
+                    }, 300);
+                };
+            }
+        }
+    }).observe(document.body,{childList:true,subtree:true}));
+})();
